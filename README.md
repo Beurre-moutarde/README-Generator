84f9ea425d305ea8017ed0ab787072e2177798ff
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-01-31 12:59:00 +0100

    mvp

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
-# Readme-Generator 
- It creates a high-quality README for the app. 
- ## Author 
- Christopher 
- ## Contact 
- Email: Chris.m.lebreton@gmail.com
+
+        # Coffee
+        [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+        
+        ## Description
+        Tea is better
+        
+        ## Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        - [Contributing](#contributing)
+        - [Tests](#tests)
+        - [Questions](#questions)
+        
+        ## Installation
+        Boil the kettle
+        
+        ## Usage
+        Add milk
+        
+        ## License
+        This project is licensed under the MIT license.
+        
+        ## Contributing
+        English crowd
+        
+        ## Tests
+        run npm test/ node index.js
+        
+        ## Questions
+        If you have any questions, feel free to reach out to me on GitHub at [Beurre-moutarde](https://github.com/Beurre-moutarde) or by email at chris@g.com. 
--- index.js
+++ index.js
@@ -1,56 +1,113 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//Write prompt with Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'projectName',
-        message: 'What is the name of your project?'
-    },
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'Please provide a brief description of your project:'
-    },
-    {
-        type: "input",
-        name: 'Table of Contents',
-        message: 'Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions',
-    },
-    {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please provide detailed instructions on how to install the software/app and any necessary information',
-    },
-    {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please provide clear and concise instructions on how to use the software, including any relevant features and functionality.',
-    },
-    {
-        type: 'List',
-        name: 'License',
-        message: 'MIT License, GNU General Public License, Apache License 2.0, BSD 3-Clause License',
-    },
-    {
-        type: 'input',
-        name: 'Contributing',
-        message: 'Please provide information on how others can contribute to your project.'
-    },
-    {
-        type: 'input',
-        name: 'Tests',
-        message: 'Please provide information on how to run tests for your project.'
-    },
-    {
-        type: 'input',
-        name: 'Questions',
-        message: 'Please provide information on how users can reach you with additional questions about your project.'
-    },
 
-]).then(answers => {
-    const readme = `# ${answers.projectName} \n ${answers.Description} \n ## Author \n ${answers.author} \n ## Contact \n Email: ${answers.email}`;
-    fs.writeFileSync("README.md", readme);
-});
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the title of your project:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description of your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation instructions:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter usage information:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license for your project:",
+      choices: [
+        "MIT",
+        "Apache 2.0",
+        "GPL 3.0",
+        "None"
+      ],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Enter contribution guidelines:",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Enter test instructions:",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your GitHub username:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address:",
+    },
+  ];
+
+  inquirer
+  .prompt(questions)
+  .then((answers) => {
+    const licenseBadge =
+      answers.license === "MIT"
+        ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        : answers.license === "Apache 2.0"
+        ? "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        : answers.license === "GPL 3.0"
+        ? "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+        : "";
+
+        const readme = `
+        # ${answers.title}
+        ${licenseBadge}
+        
+        ## Description
+        ${answers.description}
+        
+        ## Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        - [Contributing](#contributing)
+        - [Tests](#tests)
+        - [Questions](#questions)
+        
+        ## Installation
+        ${answers.installation}
+        
+        ## Usage
+        ${answers.usage}
+        
+        ## License
+        This project is licensed under the ${answers.license} license.
+        
+        ## Contributing
+        ${answers.contributing}
+        
+        ## Tests
+        ${answers.tests}
+        
+        ## Questions
+        If you have any questions, feel free to reach out to me on GitHub at [${answers.username}](https://github.com/${answers.username}) or by email at ${answers.email}. `; 
+       
+        writeToFile("README.md", readme)
+  });
+
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) throw err;
+      console.log(`The file "${fileName}" has been saved!`);
+    });
+  }
 
